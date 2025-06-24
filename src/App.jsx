6a453181193cac8046b82ab3981b81bd4b2e8abd
Vivek Jha <progamervivek2020@gmail.com>
2025-06-25 00:39:04 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import img from './assets/bg2.jpg'
-import Navbar from './components/Navbar.jsx'
-import Card from './components/Card.jsx'
+  import './App.css'
+  import img from './assets/bg2.jpg'
+  import Navbar from './components/Navbar.jsx'
+  import Card from './components/Card.jsx'
 
-function App() {
-  return (
-    <>
-       <div className="" style={{
-          backgroundImage: `url(${img})`,
-          backgroundSize: 'cover',
-          height: '100vh',
-          width: '100vw',
-       }}>
-        <Navbar />
-        <Card name="theartisticwing"/>
-       </div>
-    </>
-  )
-}
+  function App() {
+    return (
+      <div
+        className="w-[100vw] h-[100vh] bg-cover bg-no-repeat relative"
+        style={{ backgroundImage: `url(${img})` }}
+      >
+        {/* Wrap Navbar + Card in a group */}
 
-export default App
+        <div className="group inline-block relative">
+  <Navbar />
+
+  <div className="absolute top-[30vh] left-[10vw] opacity-0 scale-95 group-hover:opacity-100 group-hover:scale-100 transition-all duration-150 ease-in-out pointer-events-none group-hover:pointer-events-auto">
+    <Card name="theartisticwing" />
+  </div>
+</div>
+
+      </div>
+    )
+  }
+
+  export default App
